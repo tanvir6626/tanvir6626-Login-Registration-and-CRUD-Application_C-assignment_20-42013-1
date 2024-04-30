c37db81f3c46586ff7d2f36cc56d4393cad0b328
tanvir6626 <97705548+tanvir6626@users.noreply.github.com>
2024-04-30 11:42:54 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable("table");
+        int index;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("ID", Type.GetType("System.Int32"));
+            table.Columns.Add("firstname", Type.GetType("System.String"));
+            table.Columns.Add("lastname", Type.GetType("System.String"));
+            table.Columns.Add("Sum", Type.GetType("System.Double"));
+            dataGridView1.DataSource = table;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+            textBox3.Text = String.Empty;
+            textBox4.Text = String.Empty;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            index = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[index];
+            textBox1.Text = row.Cells[0].Value.ToString();
+            textBox2.Text = row.Cells[1].Value.ToString();
+            textBox3.Text = row.Cells[2].Value.ToString();
+            textBox4.Text = row.Cells[3].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow newdata = dataGridView1.Rows[index];
+            newdata.Cells[0].Value = textBox1.Text;
+            newdata.Cells[1].Value = textBox2.Text;
+            newdata.Cells[2].Value = textBox3.Text;
+            newdata.Cells[3].Value = textBox4.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            index = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(index);
+        }
+    }
+}
